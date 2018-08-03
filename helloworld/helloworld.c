813e32d46e8d85170e2402e9f089426c9600c6a6
Alessandro Capotondi <alessandro.capotondi@unibo.it>
2018-08-03 11:54:59 +0200

    Fixed some paths for HERO

--- helloworld/helloworld.c
+++ helloworld/helloworld.c
@@ -1,45 +1,27 @@
- //#include <stdlib.h>
- //#include <stdio.h>
- //#include <stdint.h>
- //#include <omp.h>
- //#include <time.h>   // for time measurements
- //#include <pulp-api.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <omp.h>
+#include <time.h>
+//#include <pulp-api.h>
 
- struct timespec start, stop;
- double start_ns, stop_ns, exe_time;
+struct timespec start, stop;
+double start_ns, stop_ns, exe_time;
 
-#define N 32
-#define TYPE float
-
-void vec_mult (TYPE *p, TYPE *v1, TYPE *v2)
+#pragma omp declare target
+void helloworld ()
 {
-	#pragma omp target map(to: v1[0:N], v2[0:N]) map(from: p[0:N])
-	{
-		#pragma omp parallel for
-		for (int i = 0; i < N; i++)
-			p[i] = v1[i] * v2[i];
-	}
+	#pragma omp parallel
+	printf("Hello World, I am thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
 }
-
-// void vec_mult2 ()
-// {
-// 	TYPE p[N], v1[N], v2[N];
-
-// 	#pragma omp target map(to: v1[0:N], v2[0:N]) map(from: p[0:N])
-// 	{	
-
-// 		int i = 0;
-// 		// #pragma omp parallel for
-// 		for (int i = 0; i < N; i++)
-// 			p[i] = v1[i] * v2[i];
-// 	}
-// }
+#pragma omp end declare target
 
 int main(int argc, char *argv[])
 {    
-	TYPE p[N], v1[N], v2[N];
-	vec_mult(p,v1,v2);
-	// vec_mult2();
+	#pragma omp target
+	helloworld();
+
+	helloworld();
 	return 0;
 }
 
